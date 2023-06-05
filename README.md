794dee864335b320b71d774f49b48986461e39bd
Guilherme <guilhermelegramante@gmail.com>
2023-06-05 15:59:01 -0300

    Arquivos base do projeto

---
+++ README.md
@@ -0,0 +1,9 @@
+# Cantina Store - ERP
+
+<hr>
+
+## 💻 Resumo das Versões
+
+- 1.0.0 (05/06/2023) - Projeto Limpo.
+
+<hr>
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/login', 'AuthController@loginView')->name('loginView');
+Route::post('/login', 'AuthController@login')->name('login');
+Route::get('/sair', 'AuthController@logout')->name('logout');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', 'MainController@dashboard')->name('dashboard');
+
+    Route::prefix('/usuario')->group(function () {
+        Route::get('/', 'UserController@table')->name('user.table');
+        Route::get('/inclusao', 'UserController@form')->name('user.create');
+        Route::get('/{id}', 'UserController@form')->name('user.edit');
+    });
+});
