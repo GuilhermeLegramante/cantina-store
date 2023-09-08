5f4d2c7dd0f3bcb79fadb4c001e169d23765034c
Marcelo Netto Spolaor <mspolaouro@gmail.com>
2023-09-08 11:15:58 -0300

    [+] Adicionado leitura de XML (teste).

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
+
 class TestController extends Controller
 {
     public function table()
     {
         return view('parent.test-table');
+    }
+
+    public function xml() : string {
+        $xml = Storage::disk('local')->get('Cantina-NFe.xml');
+
+        $xmlObj = simplexml_load_string($xml);
+        $json = json_encode($xmlObj);
+        $res = json_decode($json, true);
+
+        // dd($res['NFe']['infNFe']['@attributes']['Id']); // ID da NF-e
+        // dd($res['NFe']['infNFe']['ide']['cNF']); // Código sequencial da NF-e
+        dd($res['NFe']['infNFe']['det']); // Itens da NF-e
+
+        return $xml;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/teste', 'TestController@xml')->name('test');
+
 Route::get('/login', 'AuthController@loginView')->name('loginView');
 Route::post('/login', 'AuthController@login')->name('login');
 Route::get('/sair', 'AuthController@logout')->name('logout');
